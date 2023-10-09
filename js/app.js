a9e161f032859d0443822d613756574fc05385a7
marlon-morrison <ssmorrison0@gmail.com>
2023-10-09 15:28:16 -0500

    add images and js adjustments

--- js/app.js
+++ js/app.js
@@ -19,9 +19,13 @@ function goToWar(params) {
     
     
     while ((playerOne < 10) && (playerTwo < 10)){
-        var suits = ["Hearts","Diamonds","Spades","clubs"];
+        var suits = ["hearts","diamonds","spades","clubs"];
         // var cardimg = ["images/ace.jpg","","","","","","","","","","images/jack.jpg","images/queen.jpg","images/king.jpg"];              
-        var cardimg = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"];
+        var cardimg = ["ace","2","3","4","5","6","7","8","9","10","jack","queen","king"];
+        var fcardnum = 0;
+        var scardnum = 0;
+        var fsuit = 0;
+        var ssuit = 0;
         b = document.getElementById("board").innerHTML
         b = b + '<div class="row"><div class="col-sm-4" ><table id="game'+ count +'"></table></div><div class="col-sm-4" ><table id="game'+ (count+1) +'"></table></div><div class="col-sm-4" ><table id="game'+ (count+2) +'"></table></div></div>';
         document.getElementById("board").innerHTML = b;
@@ -29,6 +33,13 @@ function goToWar(params) {
         for (let i = 0; i < 3; i++) {
             oneCard = Math.floor(Math.random() * 13 + 1); 
             twoCard = Math.floor(Math.random() * 13 + 1);
+
+            fcardnum = cardimg[oneCard-1]
+            scardnum = cardimg[twoCard-1]
+
+            fsuit = suits[Math.floor(Math.random() * 3)]
+            ssuit = suits[Math.floor(Math.random() * 3)]
+
             if(oneCard > twoCard) {
                 playerOne++; 
             } else if(twoCard > oneCard ) {
@@ -36,16 +47,12 @@ function goToWar(params) {
             } else {
                 ties++;
             }
-            
-            
-            // if (oneCard == 1,11,12,13) {
-            //     card1 = cardimg[oneCard-1];
-            // } 
-            
-            // if (twoCard == 1,11,12,13) {
-            //     card2 = cardimg[twoCard-1];
-            // }
-            document.getElementById("game" + count).innerHTML = "<tr><td colspan '2'>&nbsp;</td></tr> <tr><td colspan = '2'><h4>Deal Number: " + count + "</h4></td></tr><tr><td><img src='"+ card1 +"' alt=''></td><tr><td>" + name1 + "'s card: " + cardimg[oneCard-1] + " of " + suits[Math.floor(Math.random() * 3)] + " -- Score : " + playerOne + "</td><tr><td><img src='"+ card2 +"' alt=''></td><tr><td>" + name2 + "'s card: " + cardimg[twoCard-1] + " of " + suits[Math.floor(Math.random() * 3)] + " -- Score : " + playerTwo + "</td><tr><td>Number of Ties: " + ties + "</td>";
+            card1.readdir
+            card1 = fcardnum + "-of-" + fsuit;
+            card2 = scardnum + "-of-" + ssuit;
+            console.log(card1);
+
+            document.getElementById("game" + count).innerHTML = "<tr><td colspan '2'>&nbsp;</td></tr> <tr><td colspan = '2'><h4>Deal Number: " + count + "</h4></td></tr><tr><td><img src='../media/"+ card1 +".png' alt=''></td><tr><td><p><b>" + name1 + "'s card:</b> " + fcardnum + " of " + fsuit + " -- Score : " + playerOne + "</p></td><tr><td><img src='../media/"+ card2 +".png' alt=''></td><tr><td><p><b>" + name2 + "'s card:</b> " + scardnum + " of " + ssuit + " -- Score : " + playerTwo + "</p></td><tr><td>Number of Ties: " + ties + "</td>";
             
             count++;
 
